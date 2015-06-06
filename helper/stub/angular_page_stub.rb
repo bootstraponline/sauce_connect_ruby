052b5b09685f71d5b56b304f911bdde668b7b6a9
bootstraponline <code@bootstraponline.com>
2015-06-06 19:15:25 -0400

    Update stubs

--- helper/stub/angular_page_stub.rb
+++ helper/stub/angular_page_stub.rb
@@ -2,13 +2,15 @@ module Stub
   module AngularPage
     class << self
       def goto; fail('stub called!'); end
-      def visit; fail('stub called!'); end
+      def download_button; fail('stub called!'); end
+      def download_button_element; fail('stub called!'); end
+      def download_button?; fail('stub called!'); end
     end
   end
 end
 
-module Kernel
-  def angular_page
-    Stub::AngularPage
-  end
-end unless Kernel.respond_to? :angular_page
+public
+
+def angular_page
+  Stub::AngularPage
+end
