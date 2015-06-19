010de9418ae59aaf9a7191470cbff36dcdf07afa
bootstraponline <code@bootstraponline.com>
2015-06-19 14:50:43 -0400

    Fix page object methods

--- helper/spec_helper.rb
+++ helper/spec_helper.rb
@@ -1,10 +1,10 @@
 require 'rubygems'
 require 'angular_automation'
 
+WebDriverUtils.require_all_pages
+
 # Require stubs for RubyMine auto completion
 require_relative 'stub/angular_page_stub'
-
-WebDriverUtils.require_all_pages
 
 require_relative 'sauce_helper'
 require_relative 'trace_helper'
@@ -12,10 +12,11 @@ require_relative 'expect_helper'
 require_relative 'spec_helpers'
 
 spec_helpers = SpecHelpers.instance
-at_exit { spec_helpers.driver_quit rescue nil }
 
-RSpec.configure do |config|
-  config.before(:each) do
-    WebDriverUtils.define_page_methods page_module: Page, target_class: self, watir: browser
-  end
-end
+# override stub methods
+WebDriverUtils.define_page_methods page_module: ::Page,
+                                   target_class: TOPLEVEL_BINDING.eval('self'),
+                                   method: :define_method,
+                                   watir: spec_helpers.browser
+
+at_exit { spec_helpers.driver_quit rescue nil }
