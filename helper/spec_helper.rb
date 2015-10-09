14142287a5887cf4643a46d3044074bff8a7ab91
bootstraponline <code@bootstraponline.com>
2015-10-08 23:31:17 -0400

    Use SauceRSpec.update_example_caps

--- helper/spec_helper.rb
+++ helper/spec_helper.rb
@@ -15,24 +15,11 @@ require_relative 'expect_helper'
 require_relative 'spec_helpers'
 
 RSpec.configure do |config|
-  def example_caps
-    example     = RSpec.current_example
-    caps        = example.caps
-    description = example.description
-    browser     = caps[:browserName].capitalize
-    version     = caps[:platformVersion] || caps[:version]
-    platform    = caps[:platformName] || caps[:platform]
-
-    # Set Sauce Labs job_name
-    caps[:name] = [description, '-', browser, version, '-', platform].join ' '
-    caps
-  end
-
   config.before(:each) do
     if SauceRSpec.config.sauce?
       http_client         = ::Selenium::WebDriver::Remote::Http::Curb.new
       http_client.timeout = 5 * 60 # seconds
-      caps                = example_caps
+      caps                = SauceRSpec.update_example_caps
 
       # Retry failed session initialization up to a max wait of 5 minutes.
       watir_browser       = wait(5 * 60) do
