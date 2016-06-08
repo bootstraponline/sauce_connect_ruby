c158a9ebbbf78ffe92b64584ca6ac2fc4d363225
bootstraponline <code@bootstraponline.com>
2016-06-08 09:57:52 -0400

    Remove protractor. Update Gemfile

--- Gemfile
+++ Gemfile
@@ -1,10 +1,26 @@
 source 'https://rubygems.org'
 
-gem 'angular_automation', '>= 1.0.10'
+# 3rd party generic gems
 gem 'thor', '~> 0.19.1'
+gem 'net-http-persistent', '>= 2.9.4'
+gem 'bundler', '>= 1.10.6'
+gem 'coveralls', '>= 0.8.3'
+gem 'pry', '>= 0.10.2'
+gem 'rake', '>= 10.4.2'
+gem 'rubocop', '>= 0.34.2'
+gem 'webmock', '>= 1.21.0'
 
-# todo: add these to angular_automation
-gem 'net-http-persistent', '~> 2.9.4'
+# 3rd party testing gems
+gem 'rspec', '>= 3.4.0'
+gem 'rspec-retry', '>= 0.4.5'
+
+# gems I'm the owner of
 gem 'sauce_rspec', '>= 1.0.3'
 gem 'rspec_junit', '>= 4.0.2'
 gem 'sauce_documentation', '>= 0.0.4'
+gem 'test-queue-split', '>= 0.3.2'
+gem 'webdriver_utils', '>= 1.0.2'
+gem 'sauce_platforms', '>= 2.0.0'
+gem 'trace_files', '>= 1.0.0'
+gem 'rspec_flake', '>= 0.2.3'
+gem 'appium_thor', '>= 1.0.1'
--- helper/spec_helpers.rb
+++ helper/spec_helpers.rb
@@ -31,28 +31,6 @@ class SpecHelpers
     driver.extend Selenium::WebDriver::DriverExtensions::HasTouchScreen
     driver.extend Selenium::WebDriver::DriverExtensions::HasLocation
 
-    # Must activate protractor before any driver commands
-    @protractor = Protractor.new watir: browser
-
-    # Must call after Protractor.new and not before.
-    AngularWebdriver.install_rspec_helpers
-
-    # Promote SpecHelpers onto RSpec as well.
-    context         = ::RSpec::Core::ExampleGroup
-    helper_instance = self
-    self.class.instance_methods(false).each do |method_symbol|
-      context.send(:define_method, method_symbol) do |*args, &block|
-        args.length == 0 ? helper_instance.send(method_symbol, &block) :
-          helper_instance.send(method_symbol, *args, &block)
-      end
-    end
-
-    # override stub methods
-    AngularWebdriver.define_page_methods page_module:  ::Page,
-                                       target_class: context,
-                                       method:       :define_method,
-                                       watir:        browser
-
     # set script timeout for protractor client side javascript
     # https://github.com/angular/protractor/issues/117
     _60_seconds                           = 60
@@ -65,12 +43,14 @@ class SpecHelpers
     driver.manage.timeouts.page_load      = _60_seconds unless driver.browser == :safari
     driver.manage.timeouts.implicit_wait  = 0
 
+    # todo: this is a really cool stability technique that's not specific to angular.
+    #
     # sometimes elements just don't exist even though the page has loaded
     # and wait for angular has succeeded. in these situations, use client wait.
     #
     # implicit wait shouldn't ever be used. client wait is a reliable replacement.
-    driver.set_max_wait 10 # seconds
-    driver.set_max_page_wait 30 # seconds
+    # driver.set_max_wait 10 # seconds
+    # driver.set_max_page_wait 30 # seconds
 
     # set window size
     driver.manage.window.resize_to 1024, 768
